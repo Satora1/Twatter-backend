ca42529100f3c307b7124a78cf2b83749a57a4b1
Piotr Maslanka <maslanapiotr4@gmail.com>
2023-07-11 11:22:14 +0200

    Init Project

---
+++ src/main/java/com/group3/twat/TwatterApplication.java
@@ -0,0 +1,13 @@
+package com.group3.twat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwatterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group3/twat/TwatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.twat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
